c04d49d55823c77a15ff6511629dd08644adc5a1
John Kane <john@kanej.me>
2016-06-13 18:14:32 +0100

    Deal with routes not just root.

--- src/middleware/hostname-to-ipfs-rewrite-middleware.js
+++ src/middleware/hostname-to-ipfs-rewrite-middleware.js
@@ -3,7 +3,14 @@
 const rewrite = function rewrite (config) {
   return function ipfsRewriteMiddleware (req, res, next) {
     if (config.hasOwnProperty(req.hostname)) {
-      req.url = config[req.hostname]
+      const previousUrl = req.url.substring(1)
+      const ipfsRoot = config[req.hostname]
+
+      if (previousUrl === '') {
+        req.url = ipfsRoot
+      } else {
+        req.url = ipfsRoot + '/' + previousUrl
+      }
     }
 
     next()
--- test/middleware/hostname-to-ipfs-rewrite-middleware.spec.js
+++ test/middleware/hostname-to-ipfs-rewrite-middleware.spec.js
@@ -29,7 +29,7 @@ describe('Hostname Rewrite Middleware', () => {
       next = sinon.spy()
     })
 
-    describe('for a known dns entry', () => {
+    describe('for a known dns entry at the root', () => {
       beforeEach(() => {
         req.hostname = 'www.example.com'
         middleware(req, res, next)
@@ -44,7 +44,7 @@ describe('Hostname Rewrite Middleware', () => {
       })
     })
 
-    describe('for an unknown dns entry', () => {
+    describe('for a unknown dns entry at the root', () => {
       beforeEach(() => {
         req.hostname = 'www.unknown.com'
         middleware(req, res, next)
@@ -52,6 +52,22 @@ describe('Hostname Rewrite Middleware', () => {
 
       it('leaves the url untouched', () => {
         expect(req.url).to.equal('/')
+      })
+
+      it('passes control onto the next middleware', () => {
+        expect(next.calledOnce).to.equal(true)
+      })
+    })
+
+    describe('for a known dns entry at a nested page', () => {
+      beforeEach(() => {
+        req.hostname = 'www.example.com'
+        req.url = '/nested/hello.txt'
+        middleware(req, res, next)
+      })
+
+      it('rewrites the url based on the IPFS entry', () => {
+        expect(req.url).to.equal('/ipfs/QmWATWQ7fVPP2EFGu71UkfnqhYXDYH566qy47CnJDgvs8u/nested/hello.txt')
       })
 
       it('passes control onto the next middleware', () => {
