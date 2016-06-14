5c462e6c048347e111a54453579a2e2041808340
John Kane <john@kanej.me>
2016-06-14 16:20:30 +0100

    Base version in command off of package.json.

--- src/cli/commands/version.js
+++ src/cli/commands/version.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const Command = require('ronin').Command
+const fs = require('fs')
 
 module.exports = Command.extend({
   desc: 'Shows the Antaeus servers version information',
   run: () => {
+    var packageJson = JSON.parse(fs.readFileSync('./package.json'))
     // eslint-disable-next-line no-console
-    console.log('antaeus version 0.1.0')
+    console.log(`antaeus version ${packageJson.version}`)
   }
 })
