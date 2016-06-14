a34cb084538042ac60f272047a4d76ccd8b366b5
John Kane <john@kanej.me>
2016-06-14 12:31:04 +0100

    Add an option to pass the dnsConfig directly.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@
 const Antaeus = require('./antaeus')
 
 var config = {
-  port: 3001
+  port: 3001,
+  dnsConfig: null
+}
+
+if (process.argv.length === 3) {
+  config.dnsConfig = process.argv[2]
 }
 
 var server = new Antaeus(config)
